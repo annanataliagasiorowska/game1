f1d598dbf8c93a84232262bbac53c53ec680274b
Galyna <77695117+GalkaNH@users.noreply.github.com>
2022-03-31 11:56:36 +0200

    zobacz

--- onboarding.py
+++ onboarding.py
@@ -4,23 +4,27 @@ import random
 computer_wins = 0
 player_wins = 0
 games = 0
-play = 'yes'
-while play == 'yes':
-    play = input('Do you want to play the game? Type yes or no. ').lower()
-    if play == 'yes':
-        games +=1
-        print(games)
-        player_choice = input('Write paper, rock, scissors: ').lower()
-        choices = ['paper', 'rock', 'scissors']
-        computer_choice = choices[random.randint(0,2)]
-        if player_choice == 'paper' and computer_choice == 'rock' or player_choice == 'rock' and computer_choice == 'scissors' or player_choice == 'scissors' and computer_choice == 'paper':
-            player_wins += 1 
-            print('Computer chose', computer_choice,'. You won!')
-        elif player_choice == 'paper' and computer_choice == 'scissors' or player_choice == 'rock' and computer_choice == 'paper' or player_choice == 'scissors' and computer_choice == 'rock':    
-            computer_wins += 1    
-            print('Computer chose', computer_choice,'. You lost!')
-        elif player_choice == computer_choice:
-            print('You chose the same. No winner in this round!')  
-        
+play = 'y'
 
-print('Game is over. You won', player_wins, 'times out of', games,'.')
+
+while play == 'y':
+    play = input('Do you want to play the game? Type y or n. ').lower()
+    if play == 'y':
+        games += 1
+        print(games)
+        player_choice = int(input('Write (0-paper, 1-rock, 2-scissors):  '))
+        computer_choice = random.randint(0, 2)
+        if player_choice == 0 and computer_choice == 1 or player_choice == 1 and computer_choice == 2 or player_choice == 2 and computer_choice == 0:
+            player_wins += 1
+            print('You chose', player_choice,  'Computer chose',
+                  computer_choice, '. You won!')
+        elif player_choice == 0 and computer_choice == 2 or player_choice == 1 and computer_choice == 0 or player_choice == 2 and computer_choice == 1:
+            computer_wins += 1
+            print('You chose', player_choice, 'Computer chose',
+                  computer_choice, '. You lost!')
+        elif player_choice == computer_choice:
+            print('You chose the same. No winner in this round!')
+
+
+print('Game is over. You won', player_wins, ', Computer won',
+      computer_wins, ', times out of', games, '.')
